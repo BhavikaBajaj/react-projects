60aeac3cb4c4192c2877507ef9db16dcfffb86b3
bhavika <bhavikabajaj82@gmail.com>
2025-08-16 10:09:33 +0530

    Using state to load initial posts

--- social-media/src/components/PostList.jsx
+++ social-media/src/components/PostList.jsx
@@ -1,23 +1,24 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
 import WelcomeMsg from "./WelcomeMsg";
 
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListData);
+  const [dataFetched, setDataFetched] = useState(false);
 
-  const handleGetPostClick = () => {
+  if (!dataFetched) {
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
       .then((data) => {
         addInitialPosts(data.posts);
       });
-  };
+    setDataFetched(true);
+  }
+
   return (
     <>
-      {postList.length === 0 && (
-        <WelcomeMsg onGetPostClicked={handleGetPostClick} />
-      )}
+      {postList.length === 0 && <WelcomeMsg />}
       {postList.map((post) => (
         <Post key={post.id} post={post} />
       ))}
--- social-media/src/components/WelcomeMsg.jsx
+++ social-media/src/components/WelcomeMsg.jsx
@@ -1,14 +1,7 @@
-const WelcomeMsg = ({ onGetPostClicked }) => {
+const WelcomeMsg = () => {
   return (
     <center className="welcome-msg">
       <h1>There are no posts</h1>
-      <button
-        type="button"
-        className="btn btn-primary"
-        onClick={onGetPostClicked}
-      >
-        Get Posts from Server
-      </button>
     </center>
   );
 };
