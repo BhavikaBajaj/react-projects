7e1e411698deeb6f5c08028fd2dae83f152a5c5a
bhavika <bhavikabajaj82@gmail.com>
2025-08-23 19:59:56 +0530

    Counter project using react and redux

---
+++ counter-react-redux/src/store/index.js
@@ -0,0 +1,39 @@
+import { createStore } from "redux";
+
+const INITIAL_VALUE = {
+  counter: 0,
+  privacy: false,
+};
+const counterReducer = (store = INITIAL_VALUE, action) => {
+  let newStore = store;
+  if (action.type === "INCREMENT") {
+    newStore = {
+      ...store,
+      counter: store.counter + 1,
+    };
+  } else if (action.type === "DECREMENT") {
+    newStore = {
+      ...store,
+      counter: store.counter - 1,
+    };
+  } else if (action.type === "ADD") {
+    newStore = {
+      counter: store.counter + Number(action.payload.num),
+      privacy: store.privacy,
+    };
+  } else if (action.type === "SUBTRACT") {
+    newStore = {
+      counter: store.counter - Number(action.payload.num),
+      privacy: store.privacy,
+    };
+  } else if (action.type === "PRIVACY_TOGGLE") {
+    return {
+      counter: store.counter,
+      privacy: !store.privacy,
+    };
+  }
+  return newStore;
+};
+const counterStore = createStore(counterReducer);
+
+export default counterStore;
